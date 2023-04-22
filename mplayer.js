4886901dc91d7785a2164e4350c6cd7c56932aa3
yurika0501 <titizuzu0501@gmail.com>
2023-04-22 06:41:06 +0000

    [Add]選択した曲が再生できるように変更

--- mplayer.js
+++ mplayer.js
@@ -5,7 +5,10 @@ for(var i=0; i<listitems.length; i++){
     listitems[i].addEventListener('click',
         (e)=>{
             var li = e.target;// クリックされた要素を取得
-            var file = document.querySelector('audio');
+            var file = li.getAttribute('data-file');// ファイル名を取得
+            var audio = document.querySelector('audio');// audio要素を選択
+            audio.setAttribute('src', file);//src属性をセット
+            audio.play();//再生開始
         }
     );
 }
