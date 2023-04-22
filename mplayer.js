451389c8b36a32eb9e660dcd86cf533d1aa8ec3d
yurika0501 <titizuzu0501@gmail.com>
2023-04-22 09:09:38 +0000

    [Add]曲を再生か停止で絵が変わる

--- mplayer.js
+++ mplayer.js
@@ -9,6 +9,26 @@ for(var i=0; i<listitems.length; i++){
             var audio = document.querySelector('audio');// audio要素を選択
             audio.setAttribute('src', file);//src属性をセット
             audio.play();//再生開始
+            
+            var activeli = document.querySelector('.active');
+            
+            activeli.className = '';//class属性をから文字にする
+            li.className = 'active';//クリックした要素にクラス名を設定
         }
     );
 }
+
+//再生中と停止中でイラストを切り替える
+var audio = document.querySelector('audio');//audio要素を取得
+audio.addEventListener('play',//playイベントを設定
+    (e)=>{
+        var img = document.querySelector('img');//img要素を取得
+        img.setAttribute('src', 'pict_play.png');//画像を変更
+    }
+);
+audio.addEventListener('pause',
+    (e)=>{
+        var img = document.querySelector('img');
+        img.setAttribute('src', 'pict_stop.png');
+    }
+);
